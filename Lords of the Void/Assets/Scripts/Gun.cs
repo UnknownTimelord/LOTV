cb2269317b20ae40ecd5affed96d45f8da085e08
UnknownTimelord <46335506+UnknownTimelord@users.noreply.github.com>
2020-02-02 23:07:03 -0700

    More fuckery with the test fps controller

---
+++ Lords of the Void/Assets/Scripts/Gun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+	public float range = 100f;
+	public float fireRate = 1f;
+	public int shotsPer = 5;
+	
+	public Camera fpsCam;
+	public ParticleSystem muzzleFlash;
+	
+	private float nextTimeToFire = 0f;
+	
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+		{
+			nextTimeToFire = Time.time + 1f/fireRate;
+			Shoot();
+		}
+    }
+	
+	void Shoot ()
+	{
+		muzzleFlash.Play();
+		
+		RaycastHit hit;
+		if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+		{
+			Debug.Log(hit.transform.name);
+			
+			Target target = hit.transform.GetComponent<Target>();
+			if(target != null)
+			{
+				target.TakeDamage(damage);
+			}
+		}
+		
+	}
+}
