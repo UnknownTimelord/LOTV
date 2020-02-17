e726690917ec521193909a530db8c7a32280ab25
UnknownTimelord <46335506+UnknownTimelord@users.noreply.github.com>
2020-02-16 23:59:11 -0700

    Semi-Complete Null Animations
    Everything except Left and Right

--- Lords of the Void/Assets/Scripts/PlayerMovement.cs
+++ Lords of the Void/Assets/Scripts/PlayerMovement.cs
@@ -11,19 +11,24 @@ public class PlayerMovement : MonoBehaviour
 	public float sprintSpeed = 1.5f;
 	public float gravity = -9.81f;
 	public float jumpHeight = 3f;
-
 	
 	public Transform groundCheck;
 	public float groundDistance = 0.4f;
 	public LayerMask groundMask;
-
 	
 	Vector3 velocity;
 	bool isGrounded;
+	
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+    }
     
     // Update is called once per frame
     void Update()
     {
+		
 		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 		
 		if(isGrounded && velocity.y < 0)
@@ -42,6 +47,7 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
 		float z = Input.GetAxis("Vertical");
 		
+		
 		Vector3 move = transform.right * x + transform.forward * z;
 		
 		controller.Move(move * speed * Time.deltaTime);
@@ -55,5 +61,43 @@ public class PlayerMovement : MonoBehaviour
 		velocity.y += gravity * Time.deltaTime;
 		
 		controller.Move(velocity * Time.deltaTime);
+		
+		//Animations
+		if(isGrounded && velocity.y < 0)
+		{
+			animator.SetInteger("WalkCon", 0);
+			if(Input.GetKey (KeyCode.W))
+			{
+				animator.SetInteger("WalkCon", 1);
+			}
+			else
+			{
+				animator.SetInteger("WalkCon", 0);
+			}
+			if(Input.GetKey (KeyCode.S))
+			{
+				animator.SetInteger("BackwardsCon", 1);
+			}
+			else
+			{
+				animator.SetInteger("BackwardsCon", 0);
+			}
+			if(Input.GetKey (KeyCode.LeftShift))
+			{
+				if(Input.GetKey (KeyCode.W))
+				{
+				animator.SetInteger("WalkCon", 0);
+				animator.SetInteger("RunningCon", 1);
+				}
+				else
+				{
+				animator.SetInteger("RunningCon", 0);
+				}
+			}
+			else
+			{
+				animator.SetInteger("RunningCon", 0);
+			}
+		}
     }
 }
